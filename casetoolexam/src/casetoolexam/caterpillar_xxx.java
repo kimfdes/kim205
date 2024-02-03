ef9624f736ae97b6909a237713fe0ba6ee886585
kim fernamdes <kimsfdf@gmail.com>
2024-02-03 10:30:17 +0530

    comment

---
+++ casetoolexam/src/casetoolexam/caterpillar_xxx.java
@@ -0,0 +1,14 @@
+package casetoolexam;
+
+public class caterpillar_xxx {
+
+int moves,cabbages;
+
+caterpillar_xxx(int moves,int cabbages)
+{
+	this.moves=moves;
+	this.cabbages=cabbages;
+}
+	
+	
+}
